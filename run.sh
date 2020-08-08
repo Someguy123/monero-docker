660287c256fb8ef68b3c3f60f3387d5ee1ae500e
Chris (Someguy123) <info@someguy123.com>
2020-08-08 13:22:10 +0100

    add 'status', 'monitor', and 'query' commands to run.sh

--- run.sh
+++ run.sh
@@ -8,6 +8,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 source "${DIR}/lib/000_helpers.sh" &> /dev/null
+source "${DIR}/lib/010_helpers.sh" &> /dev/null
 
 [[ -f "${DIR}/.env" ]] && source "${DIR}/.env"
 
@@ -16,6 +17,7 @@ if [[ ! -f "${DIR}/.env" ]]; then
     >&2 cp -v "${DIR}/env.example" "${DIR}/.env"
 fi
 
+: ${VERBOSE=0}
 : ${DATADIR="${DIR}/data"}
 
 : ${WALLET_CONF="${DATADIR}/wallet.conf"}
@@ -28,6 +30,9 @@ fi
 : ${WALLET_NAME='mnrwallet'}
 : ${RPC_CTR='monerod'}
 : ${RPC_LOGIN='example:example'}
+: ${RPC_HOST='http://127.0.0.1:18081/json_rpc'}
+
+: ${REMOTE_RPC="http://node.moneroworld.com:18089/json_rpc"}
 
 if [[ ! -f "$MONEROD_CONF" ]]; then
     if [[ -f "$EXAMPLE_MONEROD_CONF" ]]; then
@@ -198,6 +203,211 @@ create_account() {
 
 }
 
+: ${QUERY_AUTH=1}
+
+_mnquery-help() {
+    msg yellow "\nUsage: $0 query (-v) (host=${RPC_HOST}) [method] (params=[])\n"
+    msg green "Call a JSON RPC method against either your local Monero RPC daemon / wallet, or a remote daemon / wallet\n"
+    msg bold green "Examples:\n"
+    msg green "    $0 query get_block_count       # Get the current block count of the local RPC daemon\n"
+    msg green "    $0 query -v get_block_count    # (Verbose mode) Get the current block count of the local RPC daemon\n"
+    msg green "    $0 query https://xmr.privex.io/json_rpc get_block_count    # Get the current block count of the remote RPC 'https://xmr.privex.io'\n"
+    msg green "    $0 query http://127.0.0.1:18100 get_transfers '{\"in\":true,\"account_index\":1}'  # Query the local monero wallet daemon for a list of transfers\n"
+    msg
+}
+
+mnquery() {
+    local mnhost mnmeth mnparams
+    local CURL_ARGS
+    if (( $# < 1 )); then
+        _mnquery-help; return 1
+    fi
+    CURL_ARGS=()
+    if [[ "$1" == "-v" ]]; then
+        CURL_ARGS+=("-v"); VERBOSE=1; shift
+    else
+        CURL_ARGS+=("-sS")
+    fi
+    if (( $# < 1 )); then
+        _mnquery-help; return 1
+    fi
+
+    (( QUERY_AUTH )) && CURL_ARGS+=("-u" "$RPC_LOGIN" "--digest")
+    mnparams="[]"
+    if grep -qE "^https?:" <<< "$1"; then
+        mnhost="$1" mnmeth="$2"
+    else
+        mnhost="$RPC_HOST" mnmeth="$1"
+    fi
+    (( $# > 2 )) && mnparams="$3"
+    
+    res="$(curl "${CURL_ARGS[@]}" -X POST "$mnhost" -H 'Content-Type: application/json' -d '{"jsonrpc": "2.0", "method": "'$mnmeth'", "params": '$mnparams', "id": 1 }')"
+    _ret=$?
+    if (( _ret )); then
+        >&2 msg bold red "\n [!!!] Non-zero return code from curl: $_ret - aborting."
+        >&2 msg yellow "STDOUT Response from curl:${RESET}\n\n${res}\n\n"
+        return $_ret
+    fi
+    if (( VERBOSE )); then
+        printf "%s\n" "$res"
+    else
+        jq '.' <<< "$res"
+    fi
+}
+
+q-block-count() {
+    local res
+    (( $# > 0 )) && res=$(mnquery "$1" get_block_count) || res=$(mnquery get_block_count)
+    jq -r '.result.count' <<< "$res"
+}
+
+q-version() {
+    local res
+    (( $# > 0 )) && res=$(mnquery "$1" get_version) || res=$(mnquery get_version)
+    jq -r '.result.version' <<< "$res"
+}
+
+mnr-status() {
+    local lcl_block lcl_ver rem_block rem_ver blocks_behind
+
+    msg yellow "Local RPC URL:${RESET} ${RPC_HOST}"
+    msg yellow "Remote RPC URL:${RESET} ${REMOTE_RPC}"
+
+    msg "$_LN"
+
+    msg yellow "\n [...] Loading block count + version from local rpc: ${RPC_HOST}\n"
+    lcl_block=$(q-block-count) lcl_ver=$(q-version)
+    msg yellow "\n [...] Loading block count + version from remote rpc: ${REMOTE_RPC}\n"
+    rem_block=$(q-block-count "$REMOTE_RPC") rem_ver=$(q-version "$REMOTE_RPC")
+    lcl_block=$(( lcl_block )) rem_block=$(( rem_block ))
+
+    msg "$_LN"
+    msg green "Local RPC head block:    ${RESET}$lcl_block"
+    msg green "Local RPC version:       ${RESET}$lcl_ver"
+    msg "$_LN"
+    msg green "Remote RPC head block:   ${RESET}$rem_block"
+    msg green "Remote RPC version:      ${RESET}$rem_ver"
+    msg "$_LN"
+    msg green "Blocks behind:           ${RESET}$(( rem_block - lcl_block ))"
+    msg "$_LN"
+
+}
+
+_LN="\n==============================================================================\n"
+
+: ${MONITOR_INTERVAL=5}
+
+MONITOR_INTERVAL=$((MONITOR_INTERVAL))
+
+mnr-monitor() {
+    local props head_block block_time seconds_behind time_behind
+    local blocks_synced=0 started_at="$(rfc_datetime)" starting_block=0
+    local time_since_start mins_since_start bps=0 bpm=0
+    local remote_props remote_head_block blocks_behind mins_remaining
+    #error_control 0
+    msg
+    msg bold green "--- Monero-in-a-box Sync Monitor --- \n"
+    msg bold green "Monitoring your local steemd instance\n"
+    msg bold green "Block data will update every 10 seconds, showing the block number that your node is synced up to"
+    msg bold green "the date/time that block was produced, and how far behind in days/hours/minutes that block is.\n"
+    msg bold green "After the first check, we'll also output how many blocks have been synced so far, as well as"
+    msg bold green "the estimated blocks per second (BPS) that your node is syncing by.\n"
+    #msg bold yellow "NOTE: This will not work with a replaying node. Only with a node which is synchronising.\n"
+    
+    msg "$_LN"
+
+    msg blue "To estimate how many blocks behind your Monero node is, and to give you an ETA, we compare your node's"
+    msg blue "current block number against a remote monero RPC node. You can change the remote RPC used, simply by"
+    msg blue "setting REMOTE_RPC in your .env - like so: 'REMOTE_RPC=http://monero-rpc.example.org:18081/json_rpc'\n"
+    msg yellow "Local RPC URL:${RESET} ${RPC_HOST}"
+    msg yellow "Remote RPC URL:${RESET} ${REMOTE_RPC}"
+
+    msg "$_LN"
+
+    while true; do
+        #error_control 1
+        props=$(mnquery get_block_count)
+        ret=$?
+        if (( ret != 0 )); then
+            msg bold red "Error while obtaining Local RPC global props. Will try again soon..."
+            msg "$_LN"
+            sleep "$MONITOR_INTERVAL"
+            continue
+        fi
+        head_block=$(echo "$props" | jq -r '.result.count')
+        #block_time=$(echo "$props" | jq -r '.result.time')
+        if [ -z "$head_block" ] || [[ "$head_block" == "null" ]] ; then
+            msg bold red "Local RPC head block / block time was empty. Will try again soon..."
+            msg "$_LN"
+            sleep "$MONITOR_INTERVAL"
+            continue
+        fi
+
+        current_timestamp=$(rfc_datetime)
+        #error_control 2
+        #seconds_behind=$(compare_dates "$current_timestamp" "$block_time")
+        #if (( ret != 0 )); then
+        #    msg bold red "Local RPC timestamp was invalid (err: compare_dates). Will try again soon..."
+        #    msg "$_LN"; sleep "$MONITOR_INTERVAL"; continue
+        #fi
+        #time_behind="$(human_seconds "${seconds_behind}")"
+        #if (( ret != 0 )); then
+        #    msg bold red "Local RPC timestamp was invalid (err: human_seconds). Will try again soon..."
+        #    msg "$_LN"; sleep "$MONITOR_INTERVAL"; continue
+        #fi
+        #error_control 0
+
+        msg green "Current block:               ${RESET}${head_block}"
+        #msg green "Block time:                ${block_time}"
+        #msg green "Time behind head block:    ${time_behind}"
+        msg
+
+        (( starting_block == 0 )) && starting_block="$head_block"
+
+        blocks_synced=$((head_block - starting_block))
+
+        if (( blocks_synced > 0 )); then
+            msg green "New blocks since start:      ${RESET}$blocks_synced"
+            time_since_start=$(compare_dates "$(rfc_datetime)" "$started_at")
+            bps=$((blocks_synced/time_since_start))
+            mins_since_start=$((time_since_start / 60))
+            msg green "Blocks per second:           ${RESET}$bps"
+
+            #error_control 1
+            remote_props=$(mnquery "$REMOTE_RPC" get_block_count)
+            ret=$?
+            if (( ret == 0 )); then
+                remote_head_block=$(echo "$remote_props" | jq -r '.result.count')
+                if [ -z "$remote_head_block" ] || [[ "$remote_head_block" == "null" ]]; then
+                    msg bold red "Remote RPC head block / block time was empty. Will try again soon..."
+                    msg "$_LN"
+                    sleep "$MONITOR_INTERVAL"
+                    continue
+                fi
+                msg green "Latest network block:        ${RESET}$remote_head_block ${BLUE}(from RPC $REMOTE_RPC)"
+
+                blocks_behind=$(( remote_head_block - head_block ))
+                mins_remaining=$(( (blocks_behind / bps) / 60 ))
+                msg green "Blocks behind:               ${RESET}$blocks_behind"
+                msg green "ETA til Synced:              ${RESET}$mins_remaining ${GREEN}minutes"
+                if (( mins_since_start > 0 )); then
+                    bpm=$(( blocks_synced / (time_since_start / 60) ))
+                    msg green "Blocks per minute:           ${RESET}$bpm"
+                fi
+            else
+                msg bold red "Error while obtaining Remote RPC global props. Will try again soon..."
+                msg "$_LN"
+                sleep "$MONITOR_INTERVAL"
+                continue
+            fi
+            
+            #msg
+        fi
+        msg "$_LN"
+        sleep "$MONITOR_INTERVAL"
+    done
+}
+
 
 _help() {
     msg green "Monero in a Box"
@@ -210,6 +420,9 @@ _help() {
     msg yellow "\t - create_wallet [password] [name] - Create a monero wallet with the given password and file name "
     msg yellow "\t - start - Start monero and wallet RPC - alias for 'docker-compose up -d'"
     msg yellow "\t - stop - Stop monero and wallet RPC - alias for 'docker-compose down'"
+    msg yellow "\t - monitor - Monitor your Monero RPC daemon's sync progress, compares against a remote RPC ( $REMOTE_RPC ) to give you an ETA until fully synced"
+    msg yellow "\t - status - Show the current block number + version of your Monero daemon, plus the current block / version of REMOTE_RPC ( $REMOTE_RPC )"
+    msg yellow "\t - query (-v) (host=${RPC_HOST}) [method] (params=[]) - Call a JSON RPC method against either your local Monero RPC daemon / wallet, or a remote daemon / wallet"
     msg yellow "\t - restart - Restart monero and wallet RPC - alias for 'docker-compose restart'"
     msg
 }
@@ -239,6 +452,15 @@ case $1 in
         ;;
     create_account)
         create_account "${@:2}"
+        ;;
+    query)
+        mnquery "${@:2}"
+        ;;
+    status)
+        mnr-status
+        ;;
+    monitor)
+        mnr-monitor
         ;;
     help)
         _help
